0bc52444d9c84c96fc1e43a1bc33e47c7e53390a
adriXO06 <adriandjpc@hotmail.com>
2025-06-03 11:00:22 -0500

    datos delete
    elimine tickects y prestamos ya que se hacen en procedimiento almacenadado

--- dml.sql
+++ dml.sql
@@ -5,12 +5,6 @@ INSERT INTO Tip_document (Tipo_Doc) VALUES
 ('Cédula de Extranjería');
 # Inserción de Usuarios
 CALL Usuarios ("Anderson", "Capera", "joelcapera@gmail.com", "1034596125"); 
-# Inserción de prestamos
-INSERT INTO Prestamos (Fecha_entre, Fecha_recep, Tipo_prest, Espa_public, Id_user) VALUES
-('2025-05-20 10:00:00', '2025-05-20 12:00:00', ' Cancha Deportivoa', 'cancha', "1" ),
-('2025-05-21 14:30:00', '2025-05-21 17:00:00', 'Auditorio', 'auditorio', "2"),
-('2025-05-22 09:00:00', '2025-05-22 11:00:00', 'Auditoio', 'auditorio', "3"),
-('2025-05-23 11:00:00', '2025-05-23 13:00:00', ' Cancha Deportiva', 'cancha', "4");
 #Inserción de accesorios
 INSERT INTO Accesorios (Cant, Nom_acce, Marca) VALUES
 (10, 'Cable internet', 'AmazonBasics'),
@@ -18,12 +12,6 @@ INSERT INTO Accesorios (Cant, Nom_acce, Marca) VALUES
 (8, 'Teclado', 'Microsoft'),
 (20, 'Monitor', 'Anker'),
 (12, 'Cable HDMI', 'Sony');
-#Inserción de tickets
-INSERT INTO Tickets (Id_Equi, Fecha_Repor, Estado, Id_usu) VALUES
-(201, '2025-05-20'),
-(202, '2025-05-21'),
-(203, '2025-05-22'),
-(204, '2025-05-23');
 #Inserción Roles
 INSERT INTO Roles (Nom_rol) VALUES
 ("Instructor"),
